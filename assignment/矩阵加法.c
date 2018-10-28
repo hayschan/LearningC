89d2017b5ab533f98a00e2bc2ba87e597ab0f377
Hays Chan <1800013129@pku.edu.cn>
2018-10-28 22:34:11 +0800

    Update assignment/矩阵加法.c

--- assignment/矩阵加法.c
+++ assignment/矩阵加法.c
@@ -1,33 +1,32 @@
+#include <stdio.h>
+
 int main(){
 	int n, i, j, h, k;
 	scanf("%d",&n);
-	int a[n][n], b[n][n], c[n][n];
-	for (i=0;i<n;i++){ //input // not completed
-		for (j=0;j<(n-1);j++){
-			scanf("%d ",a[i][j]);
+	int a[100][100], b[100][100], c[100][100];
+	for (i=0;i<n;i++){ //input of array a
+		for (j=0;j<n;j++){
+			scanf("%d", &a[i][j]);
 		}
-		scanf("%d\n",a[i][j]);
 	}
-	
-	for (i=0;i<n;i++){
-		for (j=0;j<(n-1);j++){
-			scanf("%d ",b[i][j]);
+	for (i=0;i<n;i++){ //input of array b
+		for (j=0;j<n;j++){
+			scanf("%d", &b[i][j]);
 		}
-		scanf("%d\n",b[i][j]);
 	}
-	
+
 	for (i=0;i<n;i++){ //calculation
 		for (j=0;j<n;j++){
 			c[i][j] = a[i][j] + b[i][j];
 		}
 	}
-	
-	for (h=0;h<n-1;h++){ //output
-		for (k=0;k<(n-1);k++){
+
+	for (h=0;h<n;h++){ //output
+		for (k=0;k<(n-1);k++){ // insert space between output number except last number in a row
 			printf("%d ",c[h][k]);
 		}
-		printf("%d\n",c[h][k]);
+		printf("%d\n",c[h][k]); // next line
 	}
-	
+
 	return 0;
 }
