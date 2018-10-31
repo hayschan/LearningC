ed5d502497c4873f1fae0a481638b5ad2a7a9e8f
Hays Chan <1800013129@pku.edu.cn>
2018-10-31 18:46:20 +0800

    Cleaner algorithn. Input bug fixed.

--- assignment/不与最大数相同的数字之和.c
+++ assignment/不与最大数相同的数字之和.c
@@ -4,27 +4,30 @@
 int main() {
   /*
   mindflow
-  find out the largest number
-  then make all elements with the number become zero
-  sum of all element
+  find out the LargestNum in the array
+  the sum is all elements that != LargestNum
+  print sum
   */
-  int n, LargestNum = 0, sum, array[100], i, j, k;
+  int n, LargestNum = 0, sum=0, array[100], i, j, k;
   scanf("%d\n",&n);
   // try malloc tonight
   for (i = 0; i < n; i++) { //input and find the largest number
-    scanf("%d ",&array[i]);
+    scanf("%d",&array[i]);
     if (array[i]>LargestNum) {
       LargestNum=array[i];
     }
   }
   for (j = 0; j < n; j++) { //assign 0 to elements with largest number
-    if (array[j]==LargestNum) {
-      array[j]=0;
+    if (array[j]!=LargestNum) {
+      sum += array[j];
     }
   }
-  for (k = 0; k < n; k++) { //add all remaining elements in the array
-    sum = sum + array[k];
+/*  for (k = 0; k < n; k++) { //add all remaining elements in the array
+    if (array[k]!=0) {
+      sum = sum + array[k];
+    }
   }
+*/
   printf("%d\n",sum);
   return 0;
 }
