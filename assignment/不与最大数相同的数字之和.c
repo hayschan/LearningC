a49d2a5267953a1a8aae181b84547c6451223c2a
Hays Chan <1800013129@pku.edu.cn>
2018-10-31 17:06:11 +0800

    Wrong output. Needed to be fixed later.

---
+++ assignment/不与最大数相同的数字之和.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  /*
+  mindflow
+  find out the largest number
+  then make all elements with the number become zero
+  sum of all element
+  */
+  int n, LargestNum = 0, sum, array[100], i, j, k;
+  scanf("%d\n",&n);
+  for (i = 0; i < n; i++) { //input and find the largest number
+    scanf("%d ",&array[i]);
+    if (array[i]>LargestNum) {
+      LargestNum=array[i];
+    }
+  }
+  for (j = 0; j < n; j++) { //assign 0 to elements with largest number
+    if (array[j]==LargestNum) {
+      array[j]=0;
+    }
+  }
+  for (k = 0; k < n; k++) { //add all remaining elements in the array
+    sum = sum + array[k];
+  }
+  printf("%d\n",sum);
+  return 0;
+}
